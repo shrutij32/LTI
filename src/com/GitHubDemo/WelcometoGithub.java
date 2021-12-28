d37e88cdbe1caa06f8ee6d746b437141d6888334
shrutij32 <shrutijoshi3241925@gmail.com>
2021-12-28 13:19:11 +0530

    First Repository

---
+++ src/com/GitHubDemo/WelcometoGithub.java
@@ -0,0 +1,8 @@
+package com.GitHubDemo;
+
+public class WelcometoGithub {
+public static void main(String[] args) {
+	System.out.println("Hello everyone!");
+	System.out.println("This is my first GitHub repository!");
+}
+}
