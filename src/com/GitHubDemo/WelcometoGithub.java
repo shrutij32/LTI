3eb4c2cc1f30dd426360c02e8a99c129eee7f6c8
shrutij32 <shrutijoshi3241925@gmail.com>
2021-12-28 13:27:42 +0530

    made changes

--- src/com/GitHubDemo/WelcometoGithub.java
+++ src/com/GitHubDemo/WelcometoGithub.java
@@ -4,5 +4,6 @@ public class WelcometoGithub {
 public static void main(String[] args) {
 	System.out.println("Hello everyone!");
 	System.out.println("This is my first GitHub repository!");
+	System.out.println("Making changes to repository");
 }
 }
